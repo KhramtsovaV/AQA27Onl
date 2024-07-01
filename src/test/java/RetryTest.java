b640e1bf6c3f56da7723f8a31c3ef44f0a14c9ac
KhramtsovaV <khramtsova.valeria@gmail.com>
2024-07-01 21:49:59 +0300

    RetryTest

--- src/test/java/RetryTest.java
+++ src/test/java/RetryTest.java
@@ -1,4 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class RetryTest { //in progress
+public class RetryTest {
+
+    @Test(retryAnalyzer = Retry.class)
+    public void testDivisionIntWithPositiveNumbers() {
+        Assert.assertEquals(Calculator.div(10,5), 2);
+    }
+
+    @Test(retryAnalyzer = Retry.class)
+    public void testDivisionDoubleWithPositiveNumbers() {
+        Assert.assertEquals(Calculator.div(10.8, 5.0),2.0377358490566038);
+    }
 
 }
