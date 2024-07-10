b56bc34f5eb7481d8e3c03847a19a4861130b8f1
KhramtsovaV <khramtsova.valeria@gmail.com>
2024-07-10 22:57:05 +0300

    hw_webDriver

--- src/test/java/InvocationTest.java
+++
@@ -1,9 +0,0 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class InvocationTest {
-    @Test(invocationCount = 10, threadPoolSize = 5, invocationTimeOut = 10000)
-    public void testDivisionIntWithPositiveNumbers() {
-        Assert.assertEquals(Calculator.div(10,5), 2);
-    }
-}
