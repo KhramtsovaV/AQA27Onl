8bb3a70e56beac2273f1450f9de0dd2b1d121588
KhramtsovaV <khramtsova.valeria@gmail.com>
2024-08-14 13:16:18 +0300

    preparatory_commit

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -5,26 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import services.BrowsersService;
-import steps.AddRemoveSteps;
-import steps.LoginSteps;
-import steps.OrderSteps;
 
 public class BaseTest {
 
     protected WebDriver driver;
-    protected LoginSteps loginStep;
-    protected AddRemoveSteps addRemoveStep;
-    protected OrderSteps orderSteps;
+
 
 
     @BeforeMethod
     public void setUp() {
         driver = new BrowsersService().getDriver();
-
-        loginStep = new LoginSteps(driver);
-        addRemoveStep = new AddRemoveSteps(driver);
-        orderSteps = new OrderSteps(driver);
-
         driver.get(ReadProperties.getUrl());
     }
 
